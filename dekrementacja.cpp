8eb812a4c2b3442fe9e32737637e32a73c7b38fa
bartdurak <projektator@gmail.com>
2022-12-23 22:47:40 +0100

    minus minus

---
+++ dekrementacja.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+        for (int i = 15; i >= 0; i--)
+        {
+            cout << i << endl;
+        }
+        cout << "Koniec wyliczania dekrementacji";
+    return 0;
+}
